5512515ed42e6245c79e41c5582fab2f51565723
MohakGangwani <60480420+MohakGangwani@users.noreply.github.com>
2020-05-23 15:40:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ def predict():
     '''
     float_features = [float(x) for x in request.form.values()]
     final_features = [np.array(float_features)]
-    prediction = model.predict(final_features)[0]
-    #prediction = "Congratulations !!! Your order won't get Back Ordered" if model.predict(final_features)[0]==0 else "I'm sorry to say this. But there is a great chance that your Order will get Back Ordered"
+    #prediction = model.predict(final_features)[0]
+    prediction = "Congratulations !!! Your order won't be Returned." if model.predict(final_features)[0]==0 else "I'm sorry to say this. But there is a great chance that your Order will be Returned."
 
     return render_template('index.html', prediction_text='{}'.format(prediction))
 
